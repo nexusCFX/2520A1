c5460a51d5aee463177d25f00dd132e124805ffb
Brandon Chester <Brandy@Brandons-MacBook-Pro-2.local>
2016-02-07 17:23:35 -0500

    V1.4

--- CIS2750/A1/testprog.c
+++ CIS2750/A1/testprog.c
@@ -1,28 +1,16 @@
 #include "calutil.h"
-#include <stdlib.h>
-#include <stdio.h>
+
 int main() {
-   // CalComp *comp;
-    CalProp *prop = malloc(sizeof(*prop));
-    CalError readStatus;
+    CalComp *comp;
+    CalStatus readStatus;
     FILE *file;
     file = fopen("test.txt", "r");
-    readStatus = parseCalProp("NAMELIST;NAME=Bill,John:NA", prop);   
-    
-    printf("Name %s\n",prop->name);
-    CalParam *travPar = prop->param;
-    while (travPar) {
-        printf("%s\n",travPar->name);
-        printf("%d values\n",travPar->nvalues);
-        for (int j = 0; j < travPar->nvalues; j++) {
-            printf("%s\n",travPar->value[j]);
-        }
-        travPar = travPar->next;
-    }
-    printf("Value %s\n",prop->value);
-      //   if (readStatus.code == OK) {
-           //  freeCalComp(comp);
-        // }
+    readStatus = readCalFile(file, &comp);   
+    printf("Error #%d. Lines %d to %d\n", readStatus.code, readStatus.linefrom,
+         readStatus.lineto);
+         if (readStatus.code == OK) {
+             freeCalComp(comp);
+         }
     fclose(file);
 
     return 1;
