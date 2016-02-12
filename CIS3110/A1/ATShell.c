d1d06e4f78ba20a240a89a076a8921a91468a4f2
Brandon Chester <Brandy@Brandons-MacBook-Pro-2.local>
2016-02-11 21:12:07 -0500

    Safety

--- CIS3110/A1/ATShell.c
+++ CIS3110/A1/ATShell.c
@@ -5,7 +5,7 @@
 #include <stdbool.h>
 #include <assert.h>
 #include <unistd.h>
-
+#include <signal.h>
 
 char **parseInput(char *input);
 int executeCommand (char **args);
@@ -13,19 +13,36 @@ void freeArgs ();
 
 int main (int argc, char* argv[]) {
     char *input = NULL;
+    char curDir[500];
+    getwd(curDir);
     char **args;
-    int status = 1;
-    do {
+    int status;
+    int isBG = false;
+    
+    while (1) {
+        pid_t pid;
         
-        printf("> ");
+        if (isBG) {
+            isBG = false;
+        }
+        getwd(curDir);
+        printf("%s>",curDir);
         size_t size = 0;
         getline(&input, &size, stdin);
         if (strlen(input) == 1 && input[0] == '\n') {
             continue;
         }
+        
         args = parseInput(input);
         status = executeCommand(args);
-    } while (status);
+        if (status == -1) {
+            isBG = true;
+        }
+        
+        while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
+            printf ("Background process[%d] ended.\n",pid);
+        }
+    }
     return 1;
 }
 
@@ -34,6 +51,7 @@ void freeArgs () {
 }
 
 char **parseInput(char *input) {
+    
     int numArgs = 0;
     
     char **args = malloc(sizeof(char*)*strlen(input));
@@ -56,6 +74,10 @@ char **parseInput(char *input) {
 }
 
 int executeCommand (char **args) {
+    int pipeIO[2];
+    int pipePos = -1;
+    char **args2 = malloc(sizeof(args));
+    
     if (args[0] == NULL) {
         return 1;
     }
@@ -68,28 +90,74 @@ int executeCommand (char **args) {
     int i = 0;
     bool isBG = false;
     while (args[i] != NULL) {
+        if (strcmp(args[i],"|")==0) {
+            pipePos = i;
+        }
         i++;
     }
     
-    if (strcmp("&",args[i-1]) == 0) {
+    
+    if ((args > 0) && strcmp("&",args[i-1]) == 0) {
         isBG = true;
+        args[i-1] = NULL;
+        i--;
     }
     
     pid_t childPID;
-    int status;
+    int status = 1;
     bool background;
+    
+   /* if (strncmp(,,2);) {
+        changeDirectory();
+        char dir[1000] = getcwd(cd, sizeof(cwd));
+        return 1;
+    }*/
     
     childPID = fork();
     if (childPID >= 0) {
         //Fork successful
         if(childPID == 0) {
-			//this is the childz
-            //use sigaction
-            if (isBG) {
-		      setpgid(0, 0);
-            }
 			//since we are the child, we want to perform exec and use up this process ID
-		    int status = execvp(args[0], args);
+            if ((i > 1) && strcmp(args[i-2],">")==0) {
+                FILE* output = fopen(args[i-1], "w");
+                args[i-2] = NULL;
+                args[i-1] = NULL;
+                dup2(fileno(output), 1);
+                fclose(output);
+                status = execvp(args[0], args); 
+            } else if ((i > 1) && strcmp(args[i-2],"<")==0) {
+                FILE* input = fopen(args[i-1], "r");
+                args[i-2] = NULL;
+                args[i-1] = NULL;
+                dup2(fileno(input), STDIN_FILENO);
+                fclose(input);
+                status = execvp(args[0], args); 
+            } else if (pipePos != -1) { 
+                for (int j = pipePos+1; j < i; j++) {
+                    args2[j-(pipePos+1)] = args[j];
+                    args[j] = NULL;
+                }
+                args[pipePos] = NULL;
+                   
+                pipe(pipeIO);
+                pid_t tempPid = fork();
+                if (tempPid == 0) {
+                   close(pipeIO[0]);
+                   dup2(pipeIO[1], 1); 
+                   close(pipeIO[1]);
+                  status = execvp(args[0], args);
+                } else {
+                    close(pipeIO[1]);
+                   dup2(pipeIO[0], 0); 
+                   close(pipeIO[0]);
+                  status = execvp(args2[0], args2);
+                }
+            } else {
+                status = execvp(args[0], args); 
+            }
+               
+            
+            
             if (status == -1) {
                 perror("ATShell");
             }
@@ -97,13 +165,10 @@ int executeCommand (char **args) {
 		} else {
             if (!isBG) {
               waitpid(childPID, &status, 0);
-            } else {
-              waitpid(-1, &status,WNOHANG);
             }
 		}
     } else {
         puts("Failed to fork");
     }
-    return 1;
-    
+    return 1;   
 }
