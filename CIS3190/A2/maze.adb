f3c15b74a97a2f54c45f6620c116fce460159ed5
Brandon Chester <bchester@mail.uoguelph.ca>
2017-02-18 01:57:20 -0500

    Renaming to maze

--- CIS3190/A2/maze.adb
+++ CIS3190/A2/maze.adb
@@ -1,7 +1,7 @@
 with Gnat.Io; use Gnat.Io;
-procedure Ada is
-    type Position;
 
+procedure maze is
+    type Position;
     -- Something that points to a Position
     type PositionPtr is access Position;
 
@@ -9,23 +9,22 @@ procedure Ada is
     type Position is record
         x: Integer;
         y: Integer;
-        previous:PositionPtr;
+        p:PositionPtr;
     end record;
 
     stack: array(Integer range 0 .. 9) of Position;
     ptr: PositionPtr;
 begin
-    stack(0) := new Position(1, 2, null);
-    stack(1) := new Position(3, 4, n0);
-    stack(2) := new Position(5, 6, n1);
-    stack(3) := new Position(7, 8, n2);
-    stack(4) := new Position(9, 10, n3);
-    stack(5) := new Position(11, 12, n4);
-    stack(6) := new Position(13, 14, n5);
-    stack(7) := new Position(15, 16, n6);
-    stack(8) := new Position(17, 18, n7);
-    stack(9) := new Position(19, 20, n8);
-    ptr := stack(9);
+    stack(0).x = 1;
+    stack(0).x = 2;
+    stack(1).x = 3;
+    stack(1).x = 4;
+    stack(2).x = 5;
+    stack(2).x = 6;
+    stack(3).x = 7;
+    stack(3).x = 8;
+
+    ptr := stack(3);
     loop
         exit when ptr = null;
         Put(ptr.x);
@@ -35,7 +34,7 @@ begin
         ptr := ptr.previous;
         exit when ptr = null;
     end loop;
-end Maze;
+end maze;
 
 
 
