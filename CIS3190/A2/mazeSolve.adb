83b295bf37a70f2879d66107807ceee3239c888c
Brandon Chester <nexusCFX@icloud.com>
2017-02-18 03:33:29 -0500

    Working stack

--- CIS3190/A2/mazeSolve.adb
+++ CIS3190/A2/mazeSolve.adb
@@ -4,7 +4,7 @@ procedure mazeSolve is
     type Position;
     type Node;
 
-    type NodePtr;
+    type NodePtr is access Node;
     type PositionPtr is access Position;
 
     type Position is record
@@ -14,15 +14,15 @@ procedure mazeSolve is
     end record;
 
     type Node is record
-        pos: Position
-        next: NodePtr
+        pos: Position;
+        next: NodePtr;
     end record;
 
     type Stack is record
         top: NodePtr;
     end record;
 
-    procedure push(s: Stack; pos: in Position) is
+    procedure push(s:in out Stack; pos: in Position) is
         newNode: NodePtr;
     begin
         newNode := new Node'(pos, null);
@@ -30,14 +30,14 @@ procedure mazeSolve is
         s.top := newNode;
     end push;
 
-    procedure pop(s: Stack; pos: out Position) is
+    procedure pop(s:in out Stack; pos: out Position) is
     begin
-        out := s.top.pos;
+        pos := s.top.pos;
         s.top := s.top.next;
     end pop;
 
     NSStack: Stack;
-    newPos: PositionPtr;
+    newPos: Position;
     Scan_Ptr: NodePtr;
     In_Int: Integer;
 begin
@@ -46,20 +46,20 @@ begin
         Put("> ");
         Get(In_Int);
         exit when In_Int = -1;
-        newPos = new Position'In_Int, 0, null);
-        push(NSStack, newPos)
+        newPos.x := In_Int;
+        push(NSStack, newPos);
 
     end loop;
 
     -- Now print out the integers (backwards.)
-    Scan_Ptr := Head;
+    Scan_Ptr := NSStack.top;
     loop
         -- If the list is entirely empty, bail out now.
         exit when Scan_Ptr = null;
 
         -- Print, go to next.
-        Put(Scan_Ptr.Data);
-        Scan_Ptr := Scan_Ptr.Next;
+        Put(Scan_Ptr.pos.x);
+        Scan_Ptr := Scan_Ptr.next;
 
         -- If there is no next, we are done.
         exit when Scan_Ptr = null;
